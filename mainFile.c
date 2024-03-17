bfce1b6e714acaf9780b5519328ee56646ceab25
Rucha-Ambaliya <rambaliyaforall@gmail.com>
2024-03-17 13:46:21 +0530

    added select and modify functions

--- mainFile.c
+++ mainFile.c
@@ -31,7 +31,7 @@ void printMenu() {
 }
 
 int getNoOfRecords(){
-    FILE *fp = fopen("File_Of_Records", "r");
+    FILE *fp = fopen("File_Of_Records.txt", "r");
     if(fp == NULL){
         printf("\n\nError opening the file\n");
         return -1;
@@ -46,7 +46,7 @@ int getNoOfRecords(){
 
 
 void insertRecord() {
-    FILE *fp = fopen("File_Of_Records", "a");
+    FILE *fp = fopen("File_Of_Records.txt", "a");
     if(fp == NULL){
         printf("\n\nError opening the file\n");
         return;
@@ -79,12 +79,12 @@ void displayRecord() {
         printf("\n\nFILE IS EMPTY\n\n");
         return;
     }
-    FILE *fp = fopen("File_Of_Records", "r");
+    FILE *fp = fopen("File_Of_Records.txt", "r");
     if(fp == NULL){
         printf("\n\nError opening the file\n");
         return;
     }
-    printf("PERSONAL FINANCE MANAGEMENT");
+    printf("\n\nPERSONAL FINANCE MANAGEMENT");
     printf("\n\n%-4s %-20s %-8s %-10s %-10s\n", "IDX", "Record Name", "Amount", "RType", "EType");
     while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
         printf("%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
@@ -93,96 +93,245 @@ void displayRecord() {
 }
 
 void searchByName(char *nameToFind){
-    // no_of_records = getNoOfRecords();
-    // int similarRecord = 0;
-    // if(no_of_records <= 0){
-    //     printf("\n\nFILE IS EMPTY\n\n");
-    //     return;
-    // }
-    // FILE *fp = fopen("File_Of_Records", "r");
-    // if(fp == NULL){
-    //     printf("\n\nError opening the file\n");
-    //     return;
-    // }
-    // printf("PERSONAL FINANCE MANAGEMENT");
-    // printf("\n\nIDX\t Name\t Amount\t RType\t\t EType\n\n");
-    // while(fread(&record, sizeof(record), 1, fp)){
-    //     if(record.name == nameToFind){
-    //         printf("%d\t %s\t %d\t %s\t %s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
-    //         similarRecord++;
-    //     }
-    // }
-    // if (similarRecord == 0)
-    // {
-    //     printf("\n\nNo such record exist in the file\n");
-    // }
+    no_of_records = getNoOfRecords();
+    int similarRecord = 0;
+    if(no_of_records <= 0){
+        printf("\n\nFILE IS EMPTY\n\n");
+        return;
+    }
+    FILE *fp = fopen("File_Of_Records.txt", "r");
+    if(fp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    printf("\n\nPERSONAL FINANCE MANAGEMENT\n\n");
+    printf("%-4s %-20s %-8s %-10s %-10s\n", "IDX", "Record Name", "Amount", "RType", "EType");
+    while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        if(strcmp(record.name, nameToFind) == 0){
+            printf("%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+            similarRecord++;
+        }
+    }
+    if (similarRecord == 0)
+        printf("\n\nNo such record exist in the file\n");
+    else 
+        printf("\n\n%d records matched\n", similarRecord);
     
-    // fclose((fp));
+    fclose((fp));
+}
+
+void modifyName(int index){
+    no_of_records = getNoOfRecords();
+    if (index > no_of_records)
+    {
+        printf("\n\nIndex is out of bound\n");
+        return;
+    }
+    FILE *fp = fopen("File_Of_Records.txt", "r");
+    FILE *tp = fopen("temp.txt", "w");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    for(int i = 1; i < index; i++){
+        fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    
+    fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+    printf("\nEnter Name of the record: ");
+    scanf("%s", record.name);
+    fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", index, record.name, record.amount, record.recordType, record.expenseType);
+    
+    while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
+    fp = fopen("File_Of_Records.txt", "w");
+    tp = fopen("temp.txt", "r");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    while(fscanf(tp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(fp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
+}
+
+void modifyAmount(int index){
+    no_of_records = getNoOfRecords();
+    if (index > no_of_records)
+    {
+        printf("\n\nIndex is out of bound\n");
+        return;
+    }
+    FILE *fp = fopen("File_Of_Records.txt", "r");
+    FILE *tp = fopen("temp.txt", "w");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    for(int i = 1; i < index; i++){
+        fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    
+    fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+    printf("Enter the Amount: ");
+    scanf("%d", &record.amount);
+    fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", index, record.name, record.amount, record.recordType, record.expenseType);
+    
+    while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
+    fp = fopen("File_Of_Records.txt", "w");
+    tp = fopen("temp.txt", "r");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    while(fscanf(tp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(fp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
+}
+
+void modifyRtype(int index){
+    no_of_records = getNoOfRecords();
+    if (index > no_of_records)
+    {
+        printf("\n\nIndex is out of bound\n");
+        return;
+    }
+    FILE *fp = fopen("File_Of_Records.txt", "r");
+    FILE *tp = fopen("temp.txt", "w");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    for(int i = 1; i < index; i++){
+        fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    
+    fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+    printf("Enter type of the record -> Ex. INCOME / EXPENSE : ");
+    scanf("%s", record.recordType);
+    if(strcmp(record.recordType, "INCOME") == 0)
+        strcpy(record.expenseType, "NULL");
+    else if(strcmp(record.recordType, "EXPENSE") == 0){
+        printf("Type of the Expense -> Ex. NEED / WANT / INVEST : ");
+        scanf("%s", record.expenseType);
+    } 
+    else {
+        printf("\nInvalid Input\n");
+        return;
+    }
+    fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", index, record.name, record.amount, record.recordType, record.expenseType);
+    
+    while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
+    fp = fopen("File_Of_Records.txt", "w");
+    tp = fopen("temp.txt", "r");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
+    while(fscanf(tp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+        fprintf(fp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+    }
+    fclose(fp);
+    fclose(tp);
 }
 
 void modifyRecord(int index) {
-    // no_of_records = getNoOfRecords();
-    // if (index > no_of_records)
-    // {
-    //     printf("\n\nIndex is out of bound\n");
-    //     return;
-    // }
+    no_of_records = getNoOfRecords();
+    if (index > no_of_records)
+    {
+        printf("\n\nIndex is out of bound\n");
+        return;
+    }
     
-    // int choice;
-    // printf("\n\n1. Update Record Name: \n");
-    // printf("2. Update Record Type: \n");
-    // printf("3. Update Expense Type: \n");
-    // printf("4. Update all\n");
-    // printf("Enter choice: ");
-    // scanf("%d", &choice);
-    // FILE *fp = fopen("File_Of_Records.txt", "r");
-    // FILE *tp = fopen("temp.txt", "w");
+    int choice;
+    printf("\n\n1. Update Record Name: \n");
+    printf("2. Update Record Amount: \n");
+    printf("3. Update Record Type or Expense Type: \n");
+    printf("4. Update all\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+    FILE *fp = fopen("File_Of_Records.txt", "r");
+    FILE *tp = fopen("temp.txt", "w");
+    if(fp == NULL || tp == NULL){
+        printf("\n\nError opening the file\n");
+        return;
+    }
 
-    // switch (choice)
-    // {
-    // case 1:
-    //     // modifyName(index);        
-    //     break;
-    // case 2:
-    //     // modifyName(index);        
-    //     break;
-    // case 3:
-    //     // modifyName(index);        
-    //     break;
-    // case 4:
+    switch (choice)
+    {
+    case 1:
+        modifyName(index);        
+        break;
+    case 2:
+        modifyAmount(index);        
+        break;
+    case 3:
+        modifyRtype(index);        
+        break;
+    case 4:
+        for(int i = 1; i < index; i++){
+            fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+            fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+        }
         
-    //     if(fp == NULL || tp == NULL){
-    //         printf("\n\nError opening the file\n");
-    //         return;
-    //     }
-    //     for(int i = 1; i < index; i++){
-    //         fread(&record, sizeof(record), 1, fp);
-    //         fwrite(&record, sizeof(record), 1, tp);
-    //     }
-    //     insertRecord("temp.txt");
-    //     for(int i = index + 1; i <= no_of_records; i++){
-    //         fread(&record, sizeof(record), 1, fp);
-    //         fwrite(&record, sizeof(record), 1, tp);
-    //     }
-    //     fclose(fp);
-    //     fclose(tp);
-    //     FILE *fp = fopen("File_Of_Records", "w");
-    //     FILE *tp = fopen("temp.txt", "r");
-    //     if(fp == NULL || tp == NULL){
-    //         printf("\n\nError opening the file\n");
-    //         return;
-    //     }
-    //     for(int i = 0; i <= no_of_records; i++){
-    //         fread(&record, sizeof(record), 1, fp);
-    //         fwrite(&record, sizeof(record), 1, tp);
-    //     }
-    //     fclose(fp);
-    //     fclose(tp);
+        printf("\nEnter Name of the record: ");
+        scanf("%s", record.name);
+        printf("Enter the Amount: ");
+        scanf("%d", &record.amount);
+        printf("Enter type of the record -> Ex. INCOME / EXPENSE : ");
+        scanf("%s", record.recordType);
+        if(strcmp(record.recordType, "INCOME") == 0)
+            strcpy(record.expenseType, "NULL");
+        else if(strcmp(record.recordType, "EXPENSE") == 0){
+            printf("Type of the Expense -> Ex. NEED / WANT / INVEST : ");
+            scanf("%s", record.expenseType);
+        } 
+        else {
+            printf("\nInvalid Input\n");
+            return;
+        }
+        fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", index, record.name, record.amount, record.recordType, record.expenseType);
+        fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType);
+
+        while(fscanf(fp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+            fprintf(tp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+        }
+        fclose(fp);
+        fclose(tp);
+        fp = fopen("File_Of_Records.txt", "w");
+        tp = fopen("temp.txt", "r");
+        if(fp == NULL || tp == NULL){
+            printf("\n\nError opening the file\n");
+            return;
+        }
+        while(fscanf(tp, "%d\t %s\t %d\t %s\t\t %s\n", &record.idx, record.name, &record.amount, record.recordType, record.expenseType) != EOF){
+            fprintf(fp, "%-4d %-20s %-8d %-10s %-10s\n", record.idx, record.name, record.amount, record.recordType, record.expenseType);
+        }
+        fclose(fp);
+        fclose(tp);
     
-    // default:
-    //     printf("Invalid choice. Please enter a valid option.\n");
-    //     break;
-    // }
+    default:
+        printf("Invalid choice. Please enter a valid option.\n");
+        break;
+    }
 }
 
 void deleteRecord() {
@@ -207,10 +356,9 @@ void calculateLoanInterest() {
 
 int main() {
     int choice;
-    printMenu();
     do {
         printMenu();
-        printf("Enter your choice: ");
+        printf("\nEnter your choice: ");
         scanf("%d", &choice);
 
         switch (choice) {
@@ -221,13 +369,13 @@ int main() {
                 displayRecord();
                 break;
             case 3:
-                printf("\n\nEnter record name you want to find: ");
+                printf("\nEnter record name you want to find: ");
                 char str[30];
                 scanf("%s", str);
                 searchByName(str);
                 break;
             case 4:
-                printf("\n\nEnter index number of the record you want to modify: ");
+                printf("\nEnter index number of the record you want to modify: ");
                 int index;
                 scanf("%d", &index);
                 modifyRecord(index);
@@ -248,13 +396,13 @@ int main() {
                 calculateLoanInterest();
                 break;
             case 10:
-                printf("\n\nPROGRAM IS TEMINATED\n\n");
+                printf("\nPROGRAM IS TERMINATED\n\n");
                 exit(0);
                 break;
             default:
                 printf("Invalid choice. Please enter a valid option.\n");
         }
-    } while (choice != 9);
+    } while (choice != 10);
 
     return 0;
 }
